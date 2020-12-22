c86f3d79d063b4946068d8e1c9f64bbc3db6621e
Michael <mjkochka@gmail.com>
2020-12-21 16:36:32 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import checkerArray from './array.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
 
-export default App;
+  function AltSquare(props){
+    return(
+      <button className="darksquare" onClick={props.onClick}>
+        {props.value}
+      </button>
+    )
+  }
+
+  
+  class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares:  new checkerArray().squares,
+        xIsNext: true,
+      };
+    }
+  
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+
+      squares[i] = this.state.xIsNext ? <div className="circle"></div> : <div className="circleRed"></div>;
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+  
+    renderSquare(i) {
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
+    }
+
+    renderSquareAlt(i) {
+      return (
+        <AltSquare
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
+    }
+
+
+  
+    render() {
+      let counter = 0
+      let rows = []
+      for(let i = 0; i < 8; i++){
+        const cells = []
+        for (let x = 0; x < 8; x++){
+          if((counter % 2 == 0 && rows.length % 2 == 0) || (rows.length % 2 != 0 && counter % 2 != 0))
+            cells.push(this.renderSquare(counter))
+          else
+            cells.push(this.renderSquareAlt(counter))
+
+          counter = counter + 1
+        }
+        rows.push(<div className="board-row">{cells}</div>)
+      }
+
+
+      return (
+
+        <div>
+          {rows}
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  export default Game
+  
+  // ========================================
+  
---
+++ src/array.js
@@ -0,0 +1,32 @@
+import './App.css'
+
+
+class checkerArray {
+constructor(){
+
+    this.squares = [ ,<div className="circle"></div>, ,<div className="circle"></div> , , <div className="circle"></div> , , <div className="circle"></div> ,
+    <div className="circle"></div>, ,<div className="circle"></div> , , <div className="circle"></div> , , <div className="circle"></div> , ,
+    ,<div className="circle"></div>, ,<div className="circle"></div> , , <div className="circle"></div> , , <div className="circle"></div> ,
+    , , , , , , , , , , , , , , ,  ,<div className="circleRed"></div>, ,<div className="circleRed"></div> , , <div className="circleRed"></div> , , <div className="circleRed"></div> ,
+    ,  ,<div className="circleRed"></div>, ,<div className="circleRed"></div> , , <div className="circleRed"></div> , , <div className="circleRed"></div> ,
+    <div className="circleRed"></div>, ,<div className="circleRed"></div> , , <div className="circleRed"></div> , , <div className="circleRed"></div> ,
+]
+}
+/*
+initarray(){
+
+    for(let i = 1; i < 24; i + 2)
+        this.squares[i] = <div className="circle"></div>
+
+    for(let x = 40; x < 64; x + 2)
+        this.squares[x] = <div className="circleRed"></div>
+
+       
+ return this.squares
+}
+    
+*/
+
+
+}
+export default checkerArray
